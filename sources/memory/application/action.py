5923a23cc9302e4166413a9ded6f5b9db446b3db
nixion <den-kosarev@mail.ru>
2023-05-13 21:51:17 +0700

    STAGE 5

--- sources/memory/application/action.py
+++ sources/memory/application/action.py
@@ -141,6 +141,10 @@ class MemoryAction(MemoryActionSketch):
         else:
             # statistics.increase("action.execute")
             if self.scripting_language == PYTHON_LANGUAGE:
+                print("(2) CONTEXT = " + str(context))
+                print("(2) NAMESPACE = " + str(namespace))
+                print("(2) ARGS = " + str(arguments))
+                print("(2) MEMORY ACTION = " + str(self))
                 return super(MemoryAction, self).execute(context=context, namespace=namespace, arguments=arguments)
             else:
                 if namespace is None:
