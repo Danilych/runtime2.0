b4bf1fae68b022dcb978500d636e660b4616db8b
nixion <den-kosarev@mail.ru>
2023-05-13 21:42:22 +0700

    STAAGE 2

--- sources/module/manager.py
+++ sources/module/manager.py
@@ -17,13 +17,18 @@ guid_regex=re.compile("[0-9A-Z]{8}-[0-9A-Z]{4}-[0-9A-Z]{4}-[0-9A-Z]{4}-[0-9A-Z]{
 
 @contextmanager
 def start_stop_request(actions):
+
+    print("ACTION = " + str(actions) + " === " + str(type(actions)))
+
     on_request_start = actions.get("requestonstart")
+    print("ON REQUEST START = " + str(on_request_start))
     if on_request_start and on_request_start.source_code:
         managers.engine.execute(on_request_start)
 
     yield
 
     on_request_stop = actions.get("requestonstop")
+    print("ON REQUEST STOP = " + str(on_request_stop))
     if on_request_stop and on_request_stop.source_code:
         managers.engine.execute(on_request_stop)
 
